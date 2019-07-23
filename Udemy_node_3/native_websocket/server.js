4b1a63895b9367b30ff508fd3347294a25aa5004
王冠威 <jacky@hyper-light.net>
2019-07-23 17:57:05 +0800

    UP

--- Udemy_node_3/native_websocket/server.js
+++ Udemy_node_3/native_websocket/server.js
@@ -1,8 +1,59 @@
 // const http = require('http');
 const net = require('net');
+const crypto = require('crypto');//加密工具
 
+//解析表頭
+function praseHeader(str){
+	let arr = str.split('\r\n').filter(line=>line)
+	arr.shift();
+
+	let headers = {};
+	arr.forEach(element=>{
+		let [name,value] = element.split(':');
+
+		//正折去空格 轉小寫
+		name = name.replace(/^\s+|\s+$/g,'').toLowerCase();
+		value = value.replace(/^\s+|\s+$/g,'');
+
+		headers[name] = value;
+	})
+
+	return headers;
+}
+//建立伺服器
 let server = net.createServer(sock=>{
-	console.log('有人連接我')
+	sock.once('data', buffer=>{
+		let str = buffer.toString();
+
+		let headers = praseHeader(str);
+
+		//確認是否升級socket
+		if(headers['upgrade']!='websocket'){
+			console.log('no upgrade')
+			sock.end();
+
+		//確認版本
+		}else if(headers['sec-websocket-version'] != '13'){
+			console.log('no 13')
+			sock.end();
+		}else{
+
+			//加密規則
+			let key = headers['sec-websocket-key'];
+			let uuid = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+			//加密協議
+			let hash = crypto.createHash('sha1');
+
+			//更新
+			hash.update(key+uuid);
+			let key2 = hash.digest('base64');
+
+			//寫入HTTP協議
+			sock.write(`HTTP/1.1 101 Switching Protocols\r\nUpgrade:websocket\r\nConnection:upgrade\r\nSec-Websocket-Accept:${key2}\r\n\r\n`);
+			console.log('server 連線建立');
+
+		}
+	})
 
 	sock.on('end',()=>{
 		console.log('已斷開');
--- Udemy_node_3/readme.md
+++ Udemy_node_3/readme.md
@@ -42,17 +42,40 @@ formData 表單數據
 >1.性能高
 >2.雙向通信
 
-- socket.io
-	1.簡單方便
-	2.兼容IE5 (可支援低階瀏覽器)
-	3.自動數據解析
+### socket.io
+1.簡單方便
+2.兼容IE5 (可支援低階瀏覽器)
+3.自動數據解析
 
 
-- 原生WebSocket
-	握手
+### 原生WebSocket
+握手，兩人交換信息的過程
 
-客戶端| |服務器
----|---|---
- |申請→|
- |←能|
- |請求→|
+|客戶端|動作|服務器|
+|---|---|---|
+| |申請→||
+| |←能||
+| |請求→||
+
+websocket 相較HTTP Resquest Header 多了四個東西:
+
+> 擴展信息
+> Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits
+
+> 認證用的KEY，用來較驗對方是否是ws
+> Sec-WebSocket-Key: mHlw9irRpfTUjUe5IhNAoQ==
+
+> ws版本
+> Sec-WebSocket-Version: 13
+
+> 協議升級
+> Upgrade: websocket
+
+-------
+
+key => http的頭裡來
+uuid=>'258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+
+result => base64(sha1(key + uuid))
+返回這段，客戶端就知道這個是你了，這個是websocket作者定的
+
